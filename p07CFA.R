c5ea0d0c7fc5c6073f74beb40c09c3cb470e6eb9
Marq Schieber <schim20@wfu.edu>
2022-05-03 20:48:46 -0400

    Big push

---
+++ p07CFA.R
@@ -0,0 +1,34 @@
+install.packages("psych")
+library(psych)
+
+install.packages("lavaan")
+library(lavaan)
+
+install.packages("GPArotation")
+library(GPArotation)
+
+#setwd("/Volumes/labshare/Experiments/DEAR /Dialectical Beliefs Studies/Thesis big study")
+#setwd("C:/Users/marqschieber/Library/Mobile Documents/com~apple~CloudDocs/Wake Forest Psychology/Waugh Research/DEAR Data /Dialectical Emotions Scale Validation Data/DES validation data.sav")
+
+## read in posdistract data from 447 subjects for CFA
+
+dit <- read.csv("Data.csv")
+
+##get descriptives on all the items
+
+describe(dit)
+
+##exclude items with poor descriptives
+
+des2 <-data.frame(dit$DES_1NPS, dit$DES_2NPS, dit$DES_3NPS, dit$DES_4CPN, dit$DES_5PNS, dit$DES_6CPN, dit$DES_7CPN, dit$DES_8PNS,
+                  dit$DES_9PNS, dit$DES_10CPN, dit$DES_11PNS)
+## determine number of factors using parallel and VSS (velicer MAP, BIC, etc.) nusing maximum likelihood
+
+fa.parallel(des2, fm="mle", fa="fa")
+
+VSS(des2, n=3, rotate="promax", fm="ml")
+
+## factor analysis using ML, rotate = promax
+
+cfa3factorPromaxML <- fa(des2, nfactors=3, rotate="promax", fm="mle")
+
